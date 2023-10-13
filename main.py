406b40d1bad6551c73603283b5e65223e75ebee7
SofiaLimorenko <127873927+SofiaLimorenko@users.noreply.github.com>
2023-10-13 20:47:10 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+import requests
+app = FastAPI()
+@app.get('/get_data')
+async def get_data():
+    source_url ='https://github.com/SofiaLimorenko/main'
+    response = requests.get(source_url)
+    if response.status_code == 200:
+        data = response.json()
+        coordinates = data.get('coordinates')
+    destination_url = 'https://github.com/SofiaLimorenko/android'
+    response = requests.post(destination_url, json={'coordinates': coordinates})
